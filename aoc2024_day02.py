83fd3b2663210e61d2a39320e3980007fee489ae
aconconi <aconconi@users.noreply.github.com>
2025-03-14 13:01:53 +0100

    day 1 and 2 solutions

---
+++ aoc2024_day02.py
@@ -0,0 +1,56 @@
+"""
+Advent of Code 2024
+Day 02:
+"""
+
+from itertools import pairwise
+
+import pytest
+
+
+def parse_input(file_name):
+    with open(file_name, "r", encoding="ascii") as data_file:
+        return [tuple(map(int, report.split())) for report in data_file]
+
+
+def is_report_safe(report):
+    levels = report if report[0] <= report[-1] else reversed(report)
+    return all(1 <= (b - a) <= 3 for a, b in pairwise(levels))
+
+
+def day02_part1(data):
+    return sum(is_report_safe(report) for report in data)
+
+
+def day02_part2(data):
+    def gen_sub_reports(report):
+        for i in range(len(report) + 1):
+            yield report[:i] + report[i + 1 :]
+
+    return sum(
+        any(is_report_safe(sub_report) for sub_report in gen_sub_reports(report))
+        for report in data
+    )
+
+
+@pytest.fixture(autouse=True, name="test_data")
+def fixture_test_data():
+    return parse_input("data/day02_test.txt")
+
+
+def test_day02_part1(test_data):
+    assert day02_part1(test_data)
+
+
+def test_day02_part2(test_data):
+    assert day02_part2(test_data)
+
+
+if __name__ == "__main__":
+    input_data = parse_input("data/day02.txt")
+
+    print("Day 02 Part 1:")
+    print(day02_part1(input_data))  # Correct answer is 356
+
+    print("Day 02 Part 2:")
+    print(day02_part2(input_data))  # Correct answer is
