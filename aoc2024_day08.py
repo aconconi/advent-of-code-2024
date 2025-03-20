9d329652eec668420dbc65b49c06b562a59c095a
aconconi <aconconi@users.noreply.github.com>
2025-03-20 14:07:26 +0100

    Update aoc2024_day08.py

--- aoc2024_day08.py
+++ aoc2024_day08.py
@@ -4,7 +4,7 @@ Day 08:
 """
 
 from collections import defaultdict
-from itertools import combinations
+from itertools import chain, combinations, count
 
 import pytest
 
@@ -21,56 +21,42 @@ def parse_input(file_name):
         return map_freq_ant, size
 
 
-def get_antinodes(a, b, size):
-    def offset(a, b):
-        return 2 * b[0] - a[0], 2 * b[1] - a[1]
+def generate_points_part1(start, end, size):
+    dx, dy = end[0] - start[0], end[1] - start[1]
+    x, y = end[0] + dx, end[1] + dy
+    if 0 <= x < size and 0 <= y < size:
+        yield (x, y)
 
-    return {
-        pos
-        for pos in [offset(a, b), offset(b, a)]
-        if 0 <= pos[0] < size and 0 <= pos[1] < size
+
+def generate_points_part2(start, end, size):
+    dx, dy = end[0] - start[0], end[1] - start[1]
+    for k in count(1):
+        x, y = start[0] + k * dx, start[1] + k * dy
+        if 0 <= x < size and 0 <= y < size:
+            yield (x, y)
+        else:
+            break
+
+
+def solve(data, points_gen_func):
+    map_freq_ant, size = data
+    antinodes = {
+        antinode
+        for frequency, antennas in map_freq_ant.items()
+        for pair in combinations(antennas, 2)
+        for antinode in chain(
+            points_gen_func(*pair, size), points_gen_func(*pair[::-1], size)
+        )
     }
-
-
-def get_antinodes2(a, b, size):
-    x1, y1 = a
-    x2, y2 = b
-    dx = x2 - x1
-    dy = y2 - y1
-
-    def generate_points(direction):
-        k = 0 if direction == 1 else -1
-        while True:
-            x, y = x1 + k * dx, y1 + k * dy
-            if 0 <= x < size and 0 <= y < size:
-                yield (x, y)
-            else:
-                break
-            k += direction
-
-    return set(generate_points(1)) | set(generate_points(-1))
+    return len(antinodes)
 
 
 def day08_part1(data):
-    map_freq_ant, size = data
-    antinodes = {
-        antinode
-        for frequency, antennas in map_freq_ant.items()
-        for pair in combinations(antennas, 2)
-        for antinode in get_antinodes(*pair, size)
-    }
-    return len(antinodes)
+    return solve(data, generate_points_part1)
 
 
 def day08_part2(data):
-    map_freq_ant, size = data
-    antinodes = {
-        antinode
-        for frequency, antennas in map_freq_ant.items()
-        for pair in combinations(antennas, 2)
-        for antinode in get_antinodes2(*pair, size)
-    }
-    return len(antinodes)
+    return solve(data, generate_points_part2)
 
 
 @pytest.fixture(autouse=True, name="test_data")
