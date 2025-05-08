75f40e0d29fd02f669fa430b020a3a8242090341
aconconi <aconconi@users.noreply.github.com>
2025-05-08 10:06:24 +0200

    type hints

--- aoc2024_day24.py
+++ aoc2024_day24.py
@@ -15,15 +15,15 @@ class Gate:
     op: str
     output: str
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.input1, self.input2, self.op, self.output))
 
-    def is_to_be_solved(self, wires: dict):
+    def is_to_be_solved(self, wires: dict[str, bool]) -> bool:
         return (
             self.input1 in wires and self.input2 in wires and self.output not in wires
         )
 
-    def compute(self, wires: dict):
+    def compute(self, wires: dict[str, bool]) -> None:
         x, y = wires[self.input1], wires[self.input2]
         match self.op:
             case "AND":
@@ -34,12 +34,12 @@ class Gate:
                 wires[self.output] = x ^ y
 
     @classmethod
-    def from_line(cls, line: str):
+    def from_line(cls, line: str) -> "Gate":
         tokens = line.split(" ")
         return Gate(input1=tokens[0], op=tokens[1], input2=tokens[2], output=tokens[4])
 
 
-def parse_input(file_name):
+def parse_input(file_name: str) -> tuple[dict[str, bool], list[Gate]]:
     with open(file_name, "r", encoding="ascii") as data_file:
         wires, gates = map(str.splitlines, data_file.read().split("\n\n"))
     wires = {w[:3]: bool(int(w[-1])) for w in wires}
@@ -47,7 +47,7 @@ def parse_input(file_name):
     return wires, gates
 
 
-def day24_part1(data):
+def day24_part1(data: tuple[dict[str, bool], list[Gate]]) -> int:
     initial_wires, gates = data
     wires = dict(initial_wires)
     while solvable := {g for g in gates if g.is_to_be_solved(wires)}:
@@ -62,15 +62,15 @@ def day24_part1(data):
     return sum(digit * 2**i for i, digit in enumerate(digits))
 
 
-def day24_part2(data):
+def day24_part2(data: tuple[dict[str, bool], list[Gate]]) -> str:
     _, gates = data
 
     carry_wire = max(g.output for g in gates if g.output.startswith("z"))
 
-    def check_gate_op_and_inputs(v, op):
+    def check_gate_op_and_inputs(v: str, op: str) -> bool:
         return any(g.op == op and v in (g.input1, g.input2) for g in gates)
 
-    def is_wrong_gate(g: Gate):
+    def is_wrong_gate(g: Gate) -> bool:
         is_xor_with_non_xyz = g.op == "XOR" and all(
             wire[0] not in "xyz" for wire in (g.input1, g.input2, g.output)
         )
@@ -102,15 +102,15 @@ def day24_part2(data):
 
 
 @pytest.fixture(autouse=True, name="test_data")
-def fixture_test_data():
+def fixture_test_data() -> tuple[dict[str, bool], list[Gate]]:
     return parse_input("data/day24_test.txt")
 
 
-def test_day24_part1(test_data):
+def test_day24_part1(test_data: tuple[dict[str, bool], list[Gate]]) -> None:
     assert day24_part1(test_data) == 2024
 
 
-def test_day24_part2(test_data):
+def test_day24_part2(test_data: tuple[dict[str, bool], list[Gate]]) -> None:
     assert (
         day24_part2(test_data) == "ffh,mjb,rvg,tgd,wpb,z02,z03,z05,z06,z07,z08,z10,z11"
     )
